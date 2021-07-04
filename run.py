105434827bc239383df8af548d55f21abceb8a84
Joe <joebreilly@yahoo.ie>
2021-07-04 12:25:35 +0000

    template iheritance added to html files

--- run.py
+++ run.py
@@ -19,6 +19,11 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/careers") #tells flask what url should trigger the function that follows using the .route decorator
+def careers():
+    return render_template("careers.html")
+
+
 if __name__ == "__main__": # __main__ is the name of the default module in python
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
