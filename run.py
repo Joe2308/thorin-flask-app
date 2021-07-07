981878c177d8e38f559fa4423868ec30a60b49b3
Joe <joebreilly@yahoo.ie>
2021-07-07 21:28:47 +0000

    html data injected to front end from company.json

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template #We first import the class Flask capital F for class names 
 
 app = Flask(__name__) #creates instance of class name Flask and storing in a variable called app
@@ -11,17 +12,20 @@ def index():
 
 @app.route("/about") #tells flask what url should trigger the function that follows using the .route decorator
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact") #tells flask what url should trigger the function that follows using the .route decorator
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers") #tells flask what url should trigger the function that follows using the .route decorator
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__": # __main__ is the name of the default module in python
