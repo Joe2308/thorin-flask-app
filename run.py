55ad7ae0f057f63c8206d06c342db039af220b73
Joe <joebreilly@yahoo.ie>
2021-07-08 21:33:58 +0000

    Contact form routed from backend run.py and feedback provided to the user

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
 import json
-from flask import Flask, render_template #We first import the class Flask capital F for class names 
+from flask import Flask, render_template, request, flash  #We first import the class Flask capital F for class names 
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__) #creates instance of class name Flask and storing in a variable called app
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/") #tells flask what url should trigger the function that follows using the .route decorator
@@ -17,9 +20,22 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+    
 
-@app.route("/contact") #tells flask what url should trigger the function that follows using the .route decorator
+@app.route("/contact", methods=["GET", "POST"]) #tells flask what url should trigger the function that follows using the .route decorator
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
